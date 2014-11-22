82ed00939638d8511e4333f1ec39b4c4f3e40a3f
Christopher Manciero <cmanciero@gmail.com>
2014-11-21 21:47:53 -0500

    adding animation to bars

--- website/app/modules/voting/js/votingDirectives.js
+++ website/app/modules/voting/js/votingDirectives.js
@@ -35,5 +35,55 @@
 					}
 				};
 			}
+		])
+		.directive('cgmVoteBar', ['voteBarFilter',
+			function(voteBarFilter) {
+				return {
+					restrict: 'E',
+					scope: {
+						job: '=jobAttr',
+						totalVotes: '=totalVoteAttr',
+						jobIndex: '=jobIndexAttr'
+					},
+					replace: true,
+					templateUrl: '/modules/voting/views/voteBar.html',
+					link: function(scope) {
+						// get the vote total percent
+						var voteTotal = voteBarFilter(scope.job.votes, scope.totalVotes);
+						scope.votePercent = '0%';
+						scope.barClass = 'redBar';
+
+						switch (scope.jobIndex) {
+							case 0:
+								scope.barClass = 'redBar';
+								break;
+							case 1:
+								scope.barClass = 'blueBar';
+								break;
+							case 2:
+								scope.barClass = 'greenBar';
+								break;
+							case 3:
+								scope.barClass = 'purpleBar';
+								break;
+							default:
+								scope.barClass = 'redBar';
+								break;
+						}
+
+						// draws bar width
+						scope.setBarPercent = function(percent) {
+							setTimeout(function() {
+								scope.votePercent = percent + '%';
+							}, 100);
+						};
+
+						for (var i = 0, j = voteTotal; i < j; i += 1) {
+							// set the bar width, will animate to bar width
+							scope.setBarPercent(i);
+						}
+					}
+				};
+			}
 		]);
 })();
